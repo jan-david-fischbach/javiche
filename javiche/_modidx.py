9b6e8b6f12f0acdf40ee6b9327ee6667ab2502e4
Jan David Fischbach <jan-david.fischbach@blacksemicon.de>
2023-02-10 00:37:51 +0100

    Incorporate `custom_vjp` instead of jvp as suggested by Ian Williamson

--- javiche/_modidx.py
+++ javiche/_modidx.py
@@ -8,5 +8,7 @@ d = { 'settings': { 'branch': 'main',
   'syms': { 'javiche.invdes': { 'javiche.invdes.insert_mode': ('test_inverse_design.html#insert_mode', 'javiche/invdes.py'),
                                 'javiche.invdes.plot_abs': ('test_inverse_design.html#plot_abs', 'javiche/invdes.py'),
                                 'javiche.invdes.plot_real': ('test_inverse_design.html#plot_real', 'javiche/invdes.py')},
-            'javiche.jaxify': { 'javiche.jaxify.jaxit': ('jaxify.html#jaxit', 'javiche/jaxify.py'),
+            'javiche.jaxify': { 'javiche.jaxify.as_jax': ('jaxify.html#as_jax', 'javiche/jaxify.py'),
+                                'javiche.jaxify.as_numpy': ('jaxify.html#as_numpy', 'javiche/jaxify.py'),
+                                'javiche.jaxify.jaxit': ('jaxify.html#jaxit', 'javiche/jaxify.py'),
                                 'javiche.jaxify.np_cache': ('jaxify.html#np_cache', 'javiche/jaxify.py')}}}
